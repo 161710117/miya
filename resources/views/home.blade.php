f126d9d5d355005a10c6c20e15196b1e7a4f13a3
Lab RPL 1 <161710103@smkassalaambandung.sch.id>
2018-01-15 10:19:49 +0300

    gg

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body>
+<div class="jumbotron">
+        <h1>Assalamu'alaikum</h1>
+        <p>"Welcome to MyWeb"</p>
+      </div>
+</body>
+</html>
+@endsection
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body bgcolor="gold">
+<h1>Nama : Kiki Laela</h1>
+<h1>Umur : 17 tahun</h1>
+<h1>Kelas : XI-RPL 1</h1>
+<h1>Jurusan : Rekayasa Perangkat Lunak</h1>
+</body>
+</html>
+@endsection
